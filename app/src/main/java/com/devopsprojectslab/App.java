5895eb0efb540ef467a48ef84d073efe576b5e95
Maurício Sereno <mauricioisereno@gmail.com>
2025-10-27 18:40:29 -0300

    :sparkles: feat: primeiro commit do repositorio

---
+++ app/src/main/java/com/devopsprojectslab/App.java
@@ -0,0 +1,9 @@
+package com.devopsprojectslab;
+
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("🚀 Java Docker Demo rodando");
+        Thread.sleep(120);
+        System.out.println("✅ Rodou com sucesso!");
+    }
+}
